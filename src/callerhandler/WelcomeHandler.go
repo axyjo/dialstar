63068d44f1cea19ebd29873445d60c44c9b569b5
Eric Chu <e8chu@uwaterloo.ca>
2014-03-29 05:49:48 -0400

    Extracted get user count

--- src/callerhandler/WelcomeHandler.go
+++ src/callerhandler/WelcomeHandler.go
@@ -17,8 +17,25 @@ import (
 
 //Holds information about the user calling in
 
-type Marshal struct {
+type ActiveUsers struct {
 	Total int `json:"total"`
+}
+
+func GetActiveUsers() int {
+	//Marshal the say_repsonse
+	stats_url := `https://AC6f0fa1837933462d780f6fc1daf57d44:79ed2712d0cf06c87aa2783eee6aaa7a@api.twilio.com/2010-04-01/Accounts/AC6f0fa1837933462d780f6fc1daf57d44/Calls.json?Status=in-progress`
+	resp, err := http.Get(stats_url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	active_users := &ActiveUsers{}
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&active_users)
+	if err != nil {
+		panic(err)
+	}
+	return active_users.Total
 }
 
 func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,22 +61,10 @@ func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
 	//Creates a new Buffer with the initial start xml string
 	b := bytes.NewBufferString(start)
 
-	//Marshal the say_repsonse
-	stats_url := `https://AC6f0fa1837933462d780f6fc1daf57d44:79ed2712d0cf06c87aa2783eee6aaa7a@api.twilio.com/2010-04-01/Accounts/AC6f0fa1837933462d780f6fc1daf57d44/Calls.json?Status=in-progress`
-	resp, err := http.Get(stats_url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	active_users := &Marshal{}
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&active_users)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(fmt.Sprint(resp.Body))
-	fmt.Println("[META] - Current Active Users: " + fmt.Sprint(active_users.Total))
-	text := "Welcome to Dial Star! There are  " + fmt.Sprint(active_users.Total)
+	userCount := GetActiveUsers()
+
+	fmt.Println("[META] - Current Active Users: " + fmt.Sprint(userCount))
+	text := "Welcome to Dial Star! There are  " + fmt.Sprint(userCount)
 	text = text + " other users. Press star to skip a user."
 	say_response := &twiml.Say{Voice: "female", Language: "en", Loop: 1, Text: text}
 	str, err := xml.Marshal(say_response)
