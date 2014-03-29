46973b8aa9cd66d394d2ed1963cdfcfdb65fe5e9
kennethblite <cdewinte@uwaterloo.ca>
2014-03-29 02:44:35 -0300

    Added urlescaping to URL.

--- main.go
+++ main.go
@@ -45,13 +45,13 @@ func PollWaiters(c chan twiml.Thingy) {
 				accountId := "AC6f0fa1837933462d780f6fc1daf57d44"
 
 				values := make(url.Values)
-				values.Set("Url", ConfURLBase+second.Value.(twiml.Thingy).City)
+				values.Set("Url", ConfURLBase+url.QueryEscape(second.Value.(twiml.Thingy).City))
 				fmt.Println(values)
 				_, err := http.PostForm("https://"+accountId+":"+authToken+"@api.twilio.com/2010-04-01/Accounts/"+accountId+"/Calls/"+f, values)
 				if err != nil {
 					panic(err)
 				}
-				values.Set("Url", ConfURLBase+first.Value.(twiml.Thingy).City)
+				values.Set("Url", ConfURLBase+url.QueryEscape(first.Value.(twiml.Thingy).City))
 				fmt.Println(values)
 				_, err = http.PostForm("https://"+accountId+":"+authToken+"@api.twilio.com/2010-04-01/Accounts/"+accountId+"/Calls/"+s, values)
 				if err != nil {
